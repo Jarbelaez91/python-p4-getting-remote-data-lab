ad5e5709ee1b2a0ae34c6c9a1e53a28e60ed1176
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-07-31 17:58:33 -0500

    completed assignemnt

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
 import requests
 import json
 
+#Start by creating a GetRequester class.
+# This class should be able to initialize with a string URL.
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        people = json.loads(self.get_response_body())
+        return people
